3325d4b3e3965a50db34d3e89106a7fd261e8ff9
Claudia Andrea Quezada Riquelme <cquezada@tisal.local>
2021-12-24 01:27:44 -0300

    images of the last search are being stored in localStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -23,6 +23,7 @@ export class GifsService {
     //   this._historial = JSON.parse(localStorage.getItem('historial')! );
     // }
     this._historial = JSON.parse(localStorage.getItem('historial')! ) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')! )|| [];
   }
 
   buscarGifs( query: string = '' ) {
@@ -45,6 +46,7 @@ export class GifsService {
     .subscribe( ( resp ) => {
       console.log('http resp', resp.data);
       this.resultados = resp.data;
+      localStorage.setItem('resultados', JSON.stringify(this.resultados) );
     })
   }
 
